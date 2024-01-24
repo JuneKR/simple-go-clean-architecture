573b2c3cbfe989d18b5ded568a2ed20d0e520ab9
Aekkaluk Panyacharoensri <d6600128@outsource.ktb.co.th>
2024-01-24 12:01:07 +0700

    [add] fiber and db connection

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"neptune/adapters"
+	"neptune/entities"
+	"neptune/usecases"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	app := fiber.New()
+
+	db, err := gorm.Open(sqlite.Open("orders.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+
+	if err := db.AutoMigrate(&entities.Order{}); err != nil {
+		log.Fatalf("failed to migrate database: %v", err)
+	}
+
+	orderRepo := adapters.NewGormOrderRepository(db)
+	orderService := usecases.NewOrderService(orderRepo)
+	orderHandler := adapters.NewHttpOrderHandler(orderService)
+
+	app.Post("/order", orderHandler.CreateOrder)
+
+	log.Fatal(app.Listen(":8081"))
+}
